5734dfb69f7265bbe2add5479044dbf0de899caf
hoangsangnguyen <sang.nguyen@itdragons.com>
2022-08-17 20:29:56 +0700

    case study

--- case_study_module_5/angular-case-study/src/app/customer/customer-create/customer-create.component.ts
+++ case_study_module_5/angular-case-study/src/app/customer/customer-create/customer-create.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
+import {FormGroup, FormControl, Validators, ValidatorFn} from '@angular/forms';
 import {CustomerType} from '../../module/customer-type';
 import {CustomerService} from '../../service/customer.service';
 import {CustomerTypeService} from '../../service/customer-type.service';
@@ -12,6 +12,29 @@ import {CustomerTypeService} from '../../service/customer-type.service';
 export class CustomerCreateComponent implements OnInit {
   customerForm: FormGroup;
   customerTypeList: CustomerType[] = [];
+  validationMessages = {
+    name: [
+      {type: 'required', message: 'Vui lòng nhập tên'},
+      {type: 'pattern', message: 'Vui lòng nhập tên đúng'}
+    ],
+    dateOfBirth: [
+      {type: 'required', message: 'Vui lòng nhập ngày sinh'},
+    ],
+    phoneNumber: [
+      {type: 'required', message: 'Vui lòng nhập số điện thoại'},
+      // tslint:disable-next-line:max-line-length
+      {type: 'pattern', message: 'Vui lòng nhập số địa thoại đúng định dạng 090xxxxxxx hoặc 091xxxxxxx hoặc (84)+90xxxxxxx hoặc (84)+91xxxxxxx'}
+    ],
+    idCard: [
+      {type: 'required', message: 'Vui lòng nhập CMND'},
+      // tslint:disable-next-line:max-line-length
+      {type: 'pattern', message: 'Vui lòng nhập số địa thoại đúng định dạng XXXXXXXXX hoặc XXXXXXXXXXXX (X là số 0-9).'}
+    ],
+    email: [
+      {type: 'required', message: 'Vui lòng nhập email'},
+      {type: 'pattern', message: 'Vui lòng nhập email đúng định dạng abcabc@abc.abc'}
+    ]
+  };
 
   constructor(private customerService: CustomerService, private customerTypeService: CustomerTypeService) {
   }
@@ -19,12 +42,29 @@ export class CustomerCreateComponent implements OnInit {
   ngOnInit(): void {
     this.getAllCustomerType();
     this.customerForm = new FormGroup({
-      name: new FormControl(),
+      name: new FormControl('', [
+        Validators.required,
+        // tslint:disable-next-line:max-line-length
+        Validators.pattern('^[a-zA-Zàáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹđ]+(\\s[a-zA-Zàáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹđ]+)*$')
+      ]),
       gender: new FormControl(false),
-      dateOfBirth: new FormControl(),
-      idCard: new FormControl(),
-      phoneNumber: new FormControl(),
-      email: new FormControl(),
+      dateOfBirth: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^(0?[1-9]|[12][0-9]|3[01])[\\/\\-](0?[1-9]|1[012])[\\/\\-]\\d{4}$')
+      ]),
+      idCard: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^-?(0|[1-9]\\d*)?$')
+      ]),
+      phoneNumber: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^-?(0|[1-9]\\d*)?$')
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        // tslint:disable-next-line:max-line-length
+        Validators.pattern('[a-z0-9]+@[a-z]+\\.[a-z]{2,3}')
+      ]),
       address: new FormControl(),
       customerType: new FormControl(1)
     });
